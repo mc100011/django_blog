5d1d714ef4dceaf2d17079e137a1bf5f24fedd15
conde <mohamedconde1101@gmail.com>
2026-02-19 22:57:09 +0100

    Update models.py

--- accueil/models.py
+++ accueil/models.py
@@ -11,7 +11,7 @@ from django.db import models
 
 class Actualite(models.Model):
     titre = models.CharField(max_length=500)  # Augmenté pour éviter les coupures
-    contenu = models.TextField(blank=True, null=True)  # ✅ Permettre les valeurs NULL et vides
+    contenu = models.TextField(blank=True, null=True)  #  Permettre les valeurs NULL et vides
     date_publication = models.DateTimeField(auto_now_add=True)
     lien = models.URLField(max_length=500, blank=True, null=True)  # Stocke le lien original
     image_url = models.URLField(max_length=500, blank=True, null=True)  # Stocke l'URL de l'image
