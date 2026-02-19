841817e04f65a882f2ce29f3c27a6d01bb68578e
conde <mohamedconde1101@gmail.com>
2026-02-19 22:56:05 +0100

    Update settings.py

--- mon_blog/settings.py
+++ mon_blog/settings.py
@@ -93,7 +93,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Celery configuration
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://localhost:/0"
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_BEAT_SCHEDULE = {
